eb5a85dff36168cc460aa29ad8adee6e294a5b15
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-13 11:57:26 -0700

    tiny project with a list of dogs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import styles from './index.css'
 //append a ul
 //append an li with 'spot','rover','bingo','joe'
 const root = document.getElementById('root');
@@ -5,6 +6,7 @@ const root = document.getElementById('root');
 
 const testList = document.createElement('ul');
 const dogNames = ['spot', 'rover', 'bingo', 'joe'];
+testList.className = styles.dogs;
 
 dogNames.forEach(name => {
   const dogLi = document.createElement('li');

0735a0cb560731d1c9dff1c68e196920117ae82d
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-13 11:46:09 -0700

    made a list of dogs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-const template = document.createElement('template');
-template.innerHTML = `
-  <h1>TITLE THING</h1>
-  <p>destroy all humans</p>
-`;
+//append a ul
+//append an li with 'spot','rover','bingo','joe'
+const root = document.getElementById('root');
+
+
+const testList = document.createElement('ul');
+const dogNames = ['spot', 'rover', 'bingo', 'joe'];
+
+dogNames.forEach(name => {
+  const dogLi = document.createElement('li');
+  dogLi.textContent = name;
+  testList.appendChild(dogLi);
+});
+root.appendChild(testList);
